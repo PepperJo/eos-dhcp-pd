4f11ec12cefba92b0212663b07710e8d31d5b775
Jonas Pfefferle <pepperjo@japf.ch>
2021-06-30 12:31:58 +0200

    parse prefix interface option, parse prefix48

--- dhcppd.py
+++ dhcppd.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys
 import syslog
 import eossdk
@@ -6,6 +8,9 @@ import os
 import socket
 import threading
 import json
+import re
+
+prefix48Regex = re.compile("^([a-fA-F0-9]{1,4}:){1,3}:\\/48$")
 
 class dhclient:
     def __init__(self, workingDir, interface, callback):
@@ -24,7 +29,7 @@ class dhclient:
         self.sock.listen(1)
         self.callback = callback
         self.interface = interface
-        self.sockCommThread = threading.Thread(target=self.handleEvent, args=(self))
+        self.sockCommThread = threading.Thread(target=self.handleEvent)
         self.sockCommThread.start()
         
         self.scriptFilePath = workingDir + '/dhclient-script.py'
@@ -62,59 +67,123 @@ class dhcppd(eossdk.AgentHandler, eossdk.IntfHandler):
     def __init__(self, sdk, dhcpInterface, workingDir):
         self.agentMgr = sdk.get_agent_mgr()
         self.interfaceMgr = sdk.get_intf_mgr()
+        self.eapiMgr = sdk.get_eapi_mgr()
         self.tracer = eossdk.Tracer("DHCP-PD-Agent")
         eossdk.AgentHandler.__init__(self, self.agentMgr)
         eossdk.IntfHandler.__init__(self, self.interfaceMgr)
 
         for intf in self.interfaceMgr.intf_iter():
-            syslog.syslog(intf)
-        # TODO: check if interface exists
+            syslog.syslog(intf.to_string())
+        
         self.dhcpInterface = dhcpInterface
         self.workingDir = workingDir
-        self.slaIds = dict()
-        self.prefix = None
+        self.raPrefixes = dict()
+        # for now we only support one prefix per soliciting interface
+        # since we do not allow to set client DUID or IAID
+        self.delegatedPrefix = None
+        self.lock = threading.RLock()
 
         syslog.syslog("DHCP-PD Agent: constructed")
         self.tracer.trace0("Python Agent constructed")
-
-    def on_dhclient_event(self, event):
-        self.tracer.trace5("Python Agent event {}".format(event))
 
     def on_initialized(self):
         self.tracer.trace0("Initialized")
         syslog.syslog("DHCP-PD Agent Initialized")
         self.agentMgr.status_set("Status: ", "Initializing")
 
+        intf = eossdk.IntfId(self.dhcpInterface)
+        if not self.interfaceMgr.exists(intf):
+            self.tracer.trace0("Interface {} does not exist".format(self.dhcpInterface))
+            syslog.syslog("DHCP-PD Agent: Interface {} does not exist".format(self.dhcpInterface))
+            raise ValueError()
+
+        kernelInterface = self.interfaceMgr.kernel_intf_name(intf)
+        if not kernelInterface:
+            self.tracer.trace0("Interface {} does not have a kernel interface".format(self.dhcpInterface))
+            syslog.syslog("DHCP-PD Agent: Interface {} does not have a kernel interfac".format(self.dhcpInterface))
+            raise ValueError()
+
         def callback(event):
             self.on_dhclient_event(event)
-        self.dhclient = dhclient(self.workingDir, self.dhcpInterface, callback)
+        self.dhclient = dhclient(self.workingDir, kernelInterface, callback)
         self.dhclient.start()
         self.agentMgr.status_set("Status: ", "Running")
 
-    # all options are interpreted as interfaces
-    def on_agent_option(self, optionName, value):                
-        if not value:
-            self.tracer.trace3("Prefix interface {} deleted".format(optionName))
-            # TODO: remove assigned prefixes
+    def on_dhclient_event(self, event):
+        self.tracer.trace5("Python Agent event {}".format(event))
+        with self.lock:
+            pass
+
+    @staticmethod
+    def parseRaPrefixOption(value):
+        # format: <slaId 16 bit hex> <options> => see "ipv6 nd prefix" command for available options
+        splitIndex = value.find(' ')
+        if splitIndex == -1:
+            slaId = value
+            return (slaId, None)
         else:
-            # TODO: check if interface exists
-            if optionName in self.slaIds:
-                oldValue = self.slaIds[optionName]
-                if value != oldValue:
-                    pass # TODO: remove old
+            slaId = value[:splitIndex]
+            options = value[splitIndex + 1:]
+            return (slaId, options)
+
+    @staticmethod
+    def parseDelegatedPrefix48(prefix):
+        # We only support /48 delegated prefixes for now
+        if not prefix48Regex.match(prefix):
+            return None
+        doubleColonIndex = prefix.find('::')
+        prefixBase =  prefix[:doubleColonIndex]
+        groups = prefixBase.count(':') + 1
+        # append :0 to make it easy to add SLA ID later
+        for _ in range(groups, 3):
+            prefixBase.append(':0')
+        return prefixBase
+        
+    @staticmethod
+    def prefix48to64(prefix48, slaId):
+        return prefix48 + ':' + slaId + '::/64'
+
+    # all options are interpreted as RA interfaces
+    def on_agent_option(self, optionName, value):         
+        if not value:
+            # TODO: remove assigned prefixes
+            self.tracer.trace3("RA prefix interface {} deleted".format(optionName))
+        else:
+            intf = eossdk.IntfId(optionName)
+            if not self.interfaceMgr.exists(intf):
+                self.tracer.trace1("RA prefix interface {} does not exist. Ignoring.".format(optionName))
+                syslog.syslog("DHCP-PD Agent: RA prefix interface {} does not exist. Ignoring.".format(optionName))
+                return
+            
+            slaId, options = dhcppd.parseRaPrefixOption(value)
             try:
-                slaIdInt = int(value, 16)
+                slaIdInt = int(slaId, 16)
                 if slaIdInt < 1 or slaIdInt > 0xFFFF:
                     raise ValueError()
             except ValueError:
-                self.tracer.trace1("Prefix interface {} invalid SLA_ID = {}. Expecting 16bit hex value. Ignoring.".format(optionName, value))
+                self.tracer.trace1("RA prefix interface {} invalid SLA_ID = {}. Expecting 16bit hex value. Ignoring.".format(optionName, value))
                 return
-            self.slaIds[optionName] = value
-            # TODO: actually add prefix
+            
+            if optionName in self.raPrefixes:
+                slaIdOld, optionsOld = self.raPrefixes[optionName]
+                if slaId != slaIdOld:
+                    self.tracer.trace5("RA prefix interface {} remove old SLA_ID {}".format(optionName, slaIdOld))
+                    pass # TODO: remove old
+                elif options == optionsOld:
+                    # slaId and options are equal to old values => do nothing
+                    return
+                else:
+                    self.tracer.trace5("RA prefix interface {} update options \"{}\" => \"{}\"".format(optionName, optionsOld, options))
+            
+            self.raPrefixes[optionName] = (slaId, options)
+            with self.lock:
+                if self.delegatedPrefix is not None:
+                    # TODO: actually add prefix
+                    self.tracer.trace5("RA prefix interface {} add {} {}".format(optionName, slaId, options))
 
     def on_agent_enabled(self, enabled):
         if not enabled:
-            # cleanup
+            # TODO: remove all assigned prefixes
             self.dhclient.stop()
             self.agentMgr.agent_shutdown_complete_is(True)
 
@@ -122,11 +191,11 @@ class dhcppd(eossdk.AgentHandler, eossdk.IntfHandler):
 def main():
     syslog.openlog(ident="DHCP-PD-AGENT", logoption=syslog.LOG_PID, facility=syslog.LOG_LOCAL0)
     sdk = eossdk.Sdk()
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         syslog.syslog("DHCP-PD Agent invalid arguments: dhcppd.py <workingDir> <dhcpInterface>")
         sys.exit(-1)
-    workingDir = sys.argv[0]
-    dhcpInterface = sys.argv[1]
+    workingDir = sys.argv[1]
+    dhcpInterface = sys.argv[2]
     _ = dhcppd(sdk, dhcpInterface, workingDir)
     sdk.main_loop(sys.argv)
     
